a4add274b659498aead71b50f0689776f0042f63
JuliuszL <72278818+juliuszlosinski@users.noreply.github.com>
2020-10-02 23:02:36 +0200

    Add files via upload

---
+++ BirdShoter/Code/CImageBase.cs
@@ -0,0 +1,57 @@
+﻿using BirdShooter.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdShooter
+{
+    class CImageBase
+    {
+        int X, Y;
+        Bitmap bitmap;
+        public int Left { get => X; set { X = value; } }
+        public int Top { get => Y; set { Y = value; } }
+        public CImageBase(Bitmap source) => bitmap = new Bitmap(source);
+        public void DrawImage(Graphics g) => g.DrawImage(bitmap, X, Y);
+    }
+    class CBird : CImageBase
+    {
+        public Rectangle vitals = new Rectangle();
+        public CBird() : base(Resources.Bird)
+        {
+            vitals.X = Left + 30;
+            vitals.Y = Top;
+            vitals.Height = 100;
+            vitals.Width = 100;
+        }
+        public void Update(int x, int y)
+        {
+            Left = x;
+            Top = y;
+            vitals.X = Left + 30;
+            vitals.Y = Top;
+        }
+        public bool Hit(int x ,int y)
+        {
+            Rectangle hitSpot = new Rectangle(x, y, 1, 1);
+            if (vitals.Contains(hitSpot))
+                return true;
+            return false;
+        }
+    }
+    class CScoreFrame : CImageBase
+    {
+        public CScoreFrame() : base(Resources.ScoreFrame) { }
+    }
+    class CMenu : CImageBase
+    {
+        public CMenu() : base(Resources.Menu) { }
+    }
+    class CForeground : CImageBase
+    {
+        public CForeground() : base(Resources.Foreground) { }
+    }
+}
